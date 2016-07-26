c9b58da59017ae5e63fc4edc600bb5b079594ceb
Johannes Wachter <johannes.wachter@massiveart.com>
2016-07-26 16:53:28 +0200

    fixed extension tabs

--- Resources/public/js/components/articles/edit/seo/main.js
+++ Resources/public/js/components/articles/edit/seo/main.js
@@ -29,7 +29,7 @@ define(function() {
             var content = this.options.data();
             content.ext.seo = data;
 
-            this.sandbox.emit('sulu.articles.save', data, action);
+            this.sandbox.emit('sulu.articles.save', content, action);
         }
     };
 });
