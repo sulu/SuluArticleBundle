cec35da6d75b2242fe9cda09baee0db386975e37
Johannes Wachter <johannes.wachter@massiveart.com>
2016-07-27 16:26:05 +0200

    added category and tags to ongr-index (#9)
    * fixed extension tabs
    
    * added tags and categories to ongr index

--- Resources/public/js/components/articles/edit/excerpt/main.js
+++ Resources/public/js/components/articles/edit/excerpt/main.js
@@ -18,7 +18,7 @@ define([], function() {
             var content = this.options.data();
             content.ext.excerpt = data;
 
-            this.sandbox.emit('sulu.articles.save', data, action);
+            this.sandbox.emit('sulu.articles.save', content, action);
         }
     };
 });
