3a299d99a7347e8f5709b5b59d7afa966a7fb0c0
Johannes Wachter <johannes.wachter@massiveart.com>
2016-08-16 17:05:49 +0200

    immplemented by-type and by-template route-generator

---
+++ Routing/ArticleRouteGeneratorByTemplate.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\ArticleBundle\Routing;
+
+use Sulu\Bundle\ArticleBundle\Util\TypeTrait;
+use Sulu\Bundle\RouteBundle\Generator\RouteGeneratorInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Generate route for articles by type.
+ */
+class ArticleRouteGeneratorByTemplate implements RouteGeneratorInterface
+{
+    use TypeTrait;
+
+    /**
+     * @var RouteGeneratorInterface
+     */
+    private $routeGenerator;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct(RouteGeneratorInterface $routeGenerator)
+    {
+        $this->routeGenerator = $routeGenerator;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function generate($entity, array $options)
+    {
+        $template = $entity->getStructureType();
+
+        if (!array_key_exists($template, $options)) {
+            throw new \Exception(sprintf('Route-schema for template "%s" not configured!', $template));
+        }
+
+        return $this->routeGenerator->generate($entity, ['route_schema' => $options[$template]]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOptionsResolver(array $options)
+    {
+        return (new OptionsResolver())->setDefined(array_keys($options));
+    }
+}
---
+++ Routing/ArticleRouteGeneratorByType.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * This file is part of Sulu.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\ArticleBundle\Routing;
+
+use Sulu\Bundle\ArticleBundle\Util\TypeTrait;
+use Sulu\Bundle\RouteBundle\Generator\RouteGeneratorInterface;
+use Sulu\Component\Content\Metadata\Factory\StructureMetadataFactoryInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Generate route for articles by type.
+ */
+class ArticleRouteGeneratorByType implements RouteGeneratorInterface
+{
+    use TypeTrait;
+
+    /**
+     * @var RouteGeneratorInterface
+     */
+    private $routeGenerator;
+
+    /**
+     * @var StructureMetadataFactoryInterface
+     */
+    private $structureMetadataFactory;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct(
+        RouteGeneratorInterface $routeGenerator,
+        StructureMetadataFactoryInterface $structureMetadataFactory
+    ) {
+        $this->routeGenerator = $routeGenerator;
+        $this->structureMetadataFactory = $structureMetadataFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function generate($entity, array $options)
+    {
+        $type = $this->getType(
+            $this->structureMetadataFactory->getStructureMetadata('article', $entity->getStructureType())
+        );
+
+        if (!array_key_exists($type, $options)) {
+            throw new \Exception(sprintf('Route-schema for type "%s" not configured!', $type));
+        }
+
+        return $this->routeGenerator->generate($entity, ['route_schema' => $options[$type]]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOptionsResolver(array $options)
+    {
+        return (new OptionsResolver())->setDefined(array_keys($options));
+    }
+}
