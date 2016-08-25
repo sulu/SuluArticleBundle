52afba69696aec3d3b5dda528ff2026f6740d646
L91 <alex.schranz91@gmail.com>
2016-08-25 10:04:36 +0200

    Add medias with title, url and thumbnails to index (#17)
    * add medias with title, url and thumbnails to index
    
    * fix tests for media and fix author test

--- Document/MediaCollectionOngrObject.php
+++ Document/MediaCollectionOngrObject.php
@@ -11,15 +11,18 @@
 
 namespace Sulu\Bundle\ArticleBundle\Document;
 
+use ONGR\ElasticsearchBundle\Annotation\Embedded;
 use ONGR\ElasticsearchBundle\Annotation\Object;
 use ONGR\ElasticsearchBundle\Annotation\Property;
+use ONGR\ElasticsearchBundle\Collection\Collection;
+use Sulu\Bundle\MediaBundle\Api\Media;
 
 /**
  * Contains the ids and display-options.
  *
  * @Object
  */
-class MediaCollectionOngrObject
+class MediaCollectionOngrObject implements \Iterator, \ArrayAccess
 {
     /**
      * @var int[]
@@ -29,14 +32,126 @@ class MediaCollectionOngrObject
     public $ids = [];
 
     /**
+     * @var MediaOngrObject[]|Collection
+     *
+     * @Embedded(class="SuluArticleBundle:MediaOngrObject", multiple=true)
+     */
+    public $medias;
+
+    /**
      * @var string
      *
      * @Property(type="string")
      */
     public $displayOption;
 
+    /**
+     * MediaCollectionOngrObject constructor.
+     */
+    public function __construct()
+    {
+        $this->medias = new Collection();
+    }
+
+    /**
+     * Set data.
+     *
+     * @param Media[] $medias
+     * @param string $displayOption
+     */
+    public function setData($medias = [], $displayOption = 'top')
+    {
+        $this->ids = [];
+        $this->displayOption = $displayOption;
+        $this->medias = new Collection();
+
+        foreach ($medias as $media) {
+            $this->ids = $media->getId();
+
+            $mediaObject = new MediaOngrObject();
+            $mediaObject->setData($media);
+
+            $this->medias[] = $mediaObject;
+        }
+    }
+
+    /**
+     * @return int
+     */
     public function getFirst()
     {
         return reset($this->ids) ?: null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function current()
+    {
+        return $this->medias->current();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function next()
+    {
+        return $this->medias->next();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function key()
+    {
+        return $this->medias->key();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function valid()
+    {
+        return $this->medias->valid();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rewind()
+    {
+        return $this->medias->rewind();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetExists($offset)
+    {
+        return $this->medias->offsetExists($offset);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetGet($offset)
+    {
+        return $this->medias->offsetGet($offset);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetSet($offset, $value)
+    {
+        return $this->medias->offsetSet($offset, $value);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function offsetUnset($offset)
+    {
+        return $this->medias->offsetUnset($offset);
     }
 }
