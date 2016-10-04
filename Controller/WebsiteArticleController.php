ea8e40b2c7e232242b4f4a6b2afd467f33a55ae5
Patrick Hopfner <patrick.hopfner@massiveart.com>
2016-10-04 13:29:12 +0200

    Code clean up

--- Controller/WebsiteArticleController.php
+++ Controller/WebsiteArticleController.php
@@ -14,7 +14,6 @@ namespace Sulu\Bundle\ArticleBundle\Controller;
 use JMS\Serializer\SerializationContext;
 use Sulu\Bundle\ArticleBundle\Document\ArticleDocument;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -25,46 +24,23 @@ class WebsiteArticleController extends Controller
     /**
      * @param string $view
      * @param ArticleDocument $object
-     * @param Request $request
      *
      * @return Response
      */
-    public function indexAction($view, ArticleDocument $object, Request $request)
+    public function indexAction($view, ArticleDocument $object)
     {
-        if ($object->getAuthored() > new \DateTime()) {
-            throw $this->createNotFoundException();
-        }
-
-        $categoryManager = $this->get('sulu_category.category_manager');
-
-        $content = $this->resolve($object);
-        $categories = $categoryManager->findByIds($content['extension']['excerpt']['categories']);
-
-        return $this->render(
-            $view . '.html.twig',
-            array_merge(
-                [
-                    'categories' => $categoryManager->getApiObjects($categories, $request->getLocale())
-                ],
-                $content
-            )
-        );
-    }
-
-    /**
-     * @param ArticleDocument $article
-     *
-     * @return array
-     */
-    protected function resolve(ArticleDocument $article)
-    {
-        return $this->get('jms_serializer')->serialize(
-            $article,
+        $content = $this->get('jms_serializer')->serialize(
+            $object,
             'array',
             SerializationContext::create()
                 ->setSerializeNull(true)
                 ->setGroups(['website', 'content'])
                 ->setAttribute('website', true)
+        );
+
+        return $this->render(
+            $view . '.html.twig',
+            $content
         );
     }
 }
