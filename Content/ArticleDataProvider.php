e042a5225e5d67950f1deaff687a49809b7d65c0
Patrick Hopfner <patrick.hopfner@massiveart.com>
2017-02-22 17:47:07 +0100

    Removed empty line

--- Content/ArticleDataProvider.php
+++ Content/ArticleDataProvider.php
@@ -221,7 +221,6 @@ class ArticleDataProvider implements DataProviderInterface
             $search->addQuery(new TermQuery('locale', $locale));
         }
 
-
         if (array_key_exists('types', $filters) && $filters['types']) {
             $typesQuery = new BoolQuery();
             foreach ($filters['types'] as $typeFilter) {
