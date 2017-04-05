38695ec024c44066913f29262349df8d67a508d2
Johannes Wachter <johannes.wachter@massiveart.com>
2017-04-05 09:37:05 +0200

    added route-behavior to article-page

--- DependencyInjection/SuluArticleExtension.php
+++ DependencyInjection/SuluArticleExtension.php
@@ -94,6 +94,23 @@ class SuluArticleExtension extends Extension implements PrependExtensionInterfac
             );
         }
 
+        if ($container->hasExtension('sulu_route')) {
+            $container->prependExtensionConfig(
+                'sulu_route',
+                [
+                    'mappings' => [
+                        ArticlePageDocument::class => [
+                            'generator' => 'article_page',
+                            'options' => [
+                                'route_schema' => '/{translator.trans("page")}-{object.getPageNumber()}',
+                                'parent' => '{object.getParent().getRoutePath()}',
+                            ],
+                        ],
+                    ],
+                ]
+            );
+        }
+
         if ($container->hasExtension('fos_rest')) {
             $container->prependExtensionConfig(
                 'fos_rest',
