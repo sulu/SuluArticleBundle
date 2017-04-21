5aeef77294bdbffdffb522aa82075fa9e2b33734
Frank Klein Koerkamp <frank.kleinkoerkamp@webstores.nl>
2017-04-21 08:24:26 +0200

    Simplified logic for es script

--- Tests/scripts/install-travis-es.sh
+++ Tests/scripts/install-travis-es.sh
@@ -5,11 +5,11 @@ echo $ES_VERSION;
 
 if [[ "${ES_VERSION}" = "2.4.4" ]]; then
         wget https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/${ES_VERSION}/elasticsearch-${ES_VERSION}.tar.gz
-        tar -xzf elasticsearch-${ES_VERSION}.tar.gz
-        ./elasticsearch-${ES_VERSION}/bin/elasticsearch > elasticsearch.log 2>&1 &
-        wget -q --waitretry=1 --retry-connrefused -T 10 -O - http://127.0.0.1:9200
+
 else
-        curl -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ES_VERSION}.deb
-        sudo dpkg -i --force-confnew elasticsearch-${ES_VERSION}.deb
-        sudo service elasticsearch start
+        wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ES_VERSION}.tar.gz
 fi
+
+tar -xzf elasticsearch-${ES_VERSION}.tar.gz
+./elasticsearch-${ES_VERSION}/bin/elasticsearch > elasticsearch.log 2>&1 &
+wget -q --waitretry=1 --retry-connrefused -T 10 -O - http://127.0.0.1:9200
