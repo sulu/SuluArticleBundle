8841967de2b9ba2b3380a77f320abe2fb348d337
Edvinas Ilcenko <edvinas@ilcenko.com>
2018-12-06 12:38:36 +0200

    PHP 7.2 compatibility (upgraded to compatible ONGR/elasticsearch-bundle)

--- Document/SeoViewObject.php
+++ Document/SeoViewObject.php
@@ -11,13 +11,13 @@
 
 namespace Sulu\Bundle\ArticleBundle\Document;
 
-use ONGR\ElasticsearchBundle\Annotation\Object;
+use ONGR\ElasticsearchBundle\Annotation\ObjectType;
 use ONGR\ElasticsearchBundle\Annotation\Property;
 
 /**
  * Contains seo information for articles.
  *
- * @Object
+ * @ObjectType
  */
 class SeoViewObject
 {
