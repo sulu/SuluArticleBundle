250ad86678d5f8dd77de53b5e11e8b42f51d9f1e
Patrick Hopfner <patrick.hopfner@gmail.com>
2019-03-05 15:28:00 +0100

    Fix StyleCI

--- DependencyInjection/SuluArticleExtension.php
+++ DependencyInjection/SuluArticleExtension.php
@@ -220,7 +220,7 @@ class SuluArticleExtension extends Extension implements PrependExtensionInterfac
                             ],
                             'mappings' => ['SuluArticleBundle'],
                         ],
-                    ]
+                    ],
                 ]
             );
         }
