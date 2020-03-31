786e69f728a2c0676d59f1c9e815c586ba2a506c
Daniel <daniel.mathis@sulu.io>
2020-03-31 13:00:27 +0200

    update RoutablePageBehavior interface

--- Document/Behavior/RoutablePageBehavior.php
+++ Document/Behavior/RoutablePageBehavior.php
@@ -11,47 +11,11 @@
 
 namespace Sulu\Bundle\ArticleBundle\Document\Behavior;
 
-use Sulu\Bundle\RouteBundle\Model\RoutableInterface;
-use Sulu\Component\Content\Document\Behavior\StructureBehavior;
-use Sulu\Component\DocumentManager\Behavior\Mapping\LocaleBehavior;
-use Sulu\Component\DocumentManager\Behavior\Mapping\UuidBehavior;
-use Sulu\Component\Route\Document\Behavior\RoutableBehavior;
+use Sulu\Component\Route\Document\Behavior\RoutableBehavior as BaseRoutableBehavior;
 
 /**
  * This behavior has to be attached to documents which should have a sulu-route but managed by their parent.
  */
-interface RoutablePageBehavior extends RoutableInterface, UuidBehavior, LocaleBehavior, StructureBehavior, RoutableBehavior
+interface RoutablePageBehavior extends BaseRoutableBehavior
 {
-    /**
-     * Returns route-path.
-     *
-     * @return string
-     */
-    public function getRoutePath();
-
-    /**
-     * Remove route.
-     */
-    public function removeRoute();
-
-    /**
-     * Set route-path.
-     *
-     * @param string $routePath
-     */
-    public function setRoutePath($routePath);
-
-    /**
-     * Set uuid.
-     *
-     * @param string $uuid
-     */
-    public function setUuid($uuid);
-
-    /**
-     * Returns class of document without proxies.
-     *
-     * @return string
-     */
-    public function getClass();
 }
