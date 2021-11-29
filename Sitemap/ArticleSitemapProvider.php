ad682cb64f5a3fae74b664119d9d7cb23ddcbacf
Hannes Giesenow <HaDeHa@gmx.net>
2021-11-29 12:54:39 +0100

    Fixes sitemap limit for ElasticSearch 7.x

--- Sitemap/ArticleSitemapProvider.php
+++ Sitemap/ArticleSitemapProvider.php
@@ -172,6 +172,7 @@ class ArticleSitemapProvider implements SitemapProviderInterface
         }
 
         $search->addQuery($webspaceQuery);
+        $search->setTrackTotalHits(true);
 
         return $repository->findDocuments($search);
     }
