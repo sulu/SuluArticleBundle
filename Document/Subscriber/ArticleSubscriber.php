a37febdd7de41e3928a5edb3af9bc217c46cd0ff
Johannes Wachter <johannes@sulu.io>
2023-12-18 15:54:04 +0100

    index correct ghost content when deleting the locale of an article (#652)

--- Document/Subscriber/ArticleSubscriber.php
+++ Document/Subscriber/ArticleSubscriber.php
@@ -465,6 +465,17 @@ class ArticleSubscriber implements EventSubscriberInterface
             return;
         }
 
+        $concreteLocales = $this->documentInspector->getConcreteLocales($document);
+        $ghostLocale = $document->getOriginalLocale();
+        if (!\in_array($ghostLocale, $concreteLocales, true)) {
+            $ghostLocale = $concreteLocales[0] ?? null;
+        }
+
+        if (null !== $ghostLocale) {
+            /** @var ArticleDocument $document */
+            $document = $this->documentManager->find($document->getUuid(), $ghostLocale);
+        }
+
         $this->indexer->replaceWithGhostData($document, $event->getLocale());
         $this->indexer->flush();
     }
