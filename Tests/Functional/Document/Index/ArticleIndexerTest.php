5d13297bdfc2104d49ffb429f44bf505d46994d7
Alexander Schranz <alexander@sulu.io>
2024-06-07 12:34:53 +0200

    Add reproducer test to avoid sideffects to other documents

--- Tests/Functional/Document/Index/ArticleIndexerTest.php
+++ Tests/Functional/Document/Index/ArticleIndexerTest.php
@@ -121,6 +121,32 @@ class ArticleIndexerTest extends SuluTestCase
             'default_with_route'
         );
 
+        $otherArticle = $this->createArticle(
+            [
+                'article' => 'Other content',
+            ],
+            'Other Article',
+            'default_with_route'
+        );
+
+        $articleDocumentUuid = $article['id'];
+
+        $documentDE = $this->findViewDocument($articleDocumentUuid, 'de');
+        $documentEN = $this->findViewDocument($articleDocumentUuid, 'en');
+        $documentFR = $this->findViewDocument($articleDocumentUuid, 'fr');
+
+        $this->assertSame('Test Article', $documentEN->getTitle());
+        $this->assertSame('localized', $documentEN->getLocalizationState()->state);
+        $this->assertNull($documentEN->getLocalizationState()->locale);
+
+        $this->assertSame('Test Article', $documentDE->getTitle());
+        $this->assertSame('ghost', $documentDE->getLocalizationState()->state);
+        $this->assertSame('en', $documentDE->getLocalizationState()->locale);
+
+        $this->assertSame('Test Article', $documentFR->getTitle());
+        $this->assertSame('ghost', $documentFR->getLocalizationState()->state);
+        $this->assertSame('en', $documentFR->getLocalizationState()->locale);
+
         $secondLocale = 'de';
 
         // now add second locale
@@ -134,20 +160,59 @@ class ArticleIndexerTest extends SuluTestCase
             'Test Artikel Deutsch',
             'default_with_route'
         );
+        $this->updateArticle(
+            $otherArticle['id'],
+            $secondLocale,
+            [
+                'id' => $otherArticle['id'],
+                'article' => 'Anderer Inhalt',
+            ],
+            'Anderer Artikel Deutsch',
+            'default_with_route'
+        );
 
         /** @var ArticleDocument $articleDocument */
         $articleDocument = $this->documentManager->find($article['id']);
-        $this->indexer->replaceWithGhostData($articleDocument, 'de');
-        $this->indexer->flush();
+        $otherDocument = $this->documentManager->find($otherArticle['id']);
 
-        $documentDE = $this->findViewDocument($articleDocument->getUuid(), 'de');
+        // instead of calling replaceWithGhostData directly we go through the document manager to reproduce: https://github.com/sulu/SuluArticleBundle/pull/677 correctly
+        $this->documentManager->removeLocale($articleDocument, 'de');
+        $this->documentManager->removeLocale($otherDocument, 'en');
+
+        $this->documentManager->flush();
+
         $documentEN = $this->findViewDocument($articleDocument->getUuid(), 'en');
+        $documentDE = $this->findViewDocument($articleDocument->getUuid(), 'de');
+        $documentFR = $this->findViewDocument($articleDocument->getUuid(), 'fr');
+
+        $this->assertSame('Test Article', $documentEN->getTitle());
+        $this->assertSame('localized', $documentEN->getLocalizationState()->state);
+        $this->assertNull($documentEN->getLocalizationState()->locale);
 
         $this->assertSame('Test Article', $documentDE->getTitle());
         $this->assertSame('ghost', $documentDE->getLocalizationState()->state);
         $this->assertSame('en', $documentDE->getLocalizationState()->locale);
 
-        $this->assertSame('Test Article', $documentEN->getTitle());
+        $this->assertSame('Test Article', $documentFR->getTitle());
+        $this->assertSame('ghost', $documentFR->getLocalizationState()->state);
+        $this->assertSame('en', $documentFR->getLocalizationState()->locale);
+
+        /** @var ArticleDocument $otherDocument */
+        $otherDocumentEN = $this->findViewDocument($otherDocument->getUuid(), 'en');
+        $otherDocumentDE = $this->findViewDocument($otherDocument->getUuid(), 'de');
+        $otherDocumentFR = $this->findViewDocument($otherDocument->getUuid(), 'fr');
+
+        $this->assertSame('Anderer Artikel Deutsch', $otherDocumentDE->getTitle());
+        $this->assertSame('localized', $otherDocumentDE->getLocalizationState()->state);
+        $this->assertNull($otherDocumentDE->getLocalizationState()->locale);
+
+        $this->assertSame('Anderer Artikel Deutsch', $otherDocumentEN->getTitle());
+        $this->assertSame('ghost', $otherDocumentEN->getLocalizationState()->state);
+        $this->assertSame('de', $otherDocumentEN->getLocalizationState()->locale);
+
+        $this->assertSame('Anderer Artikel Deutsch', $otherDocumentFR->getTitle());
+        $this->assertSame('ghost', $otherDocumentFR->getLocalizationState()->state);
+        $this->assertSame('de', $otherDocumentFR->getLocalizationState()->locale);
     }
 
     public function testReplaceWithGhostDataUpdateExistingGhosts()
